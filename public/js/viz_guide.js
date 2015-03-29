63153cb497115b0baf3deb3015c33389dc0c9b56
Youjin Shin <meggonagul@gmail.com>
2015-03-29 18:22:35 -0400

    edit

--- public/js/viz_guide.js
+++ public/js/viz_guide.js
@@ -20,32 +20,38 @@ function drawLayers() {
 		.attr('stroke', '#fff')
 		.attr('stroke-width', 1);
 
-	var diameter2 = 35;
+	var diameter2 = 26;
+
 	// var tg = (width/4 - 110 - 5 * diameter2)/3; // for 3 circles
-	var tg = (width/4 - 110 - 25 - 3 * diameter2)/2; // for 2 circles
-	var x1 = gap + 110 + 25;
-	var x2 = x1 + 2 * diameter2 + tg;
-	var x3 = x2 + 2 * diameter2 + tg;
+	// var x1 = gap + 110;
+	// var x2 = x1 + 2 * diameter2 + tg;
+	// var x3 = x2 + 2 * diameter2 + tg;
+	
+	var tg = (width/4-40)/2; 
+	var x1 = gap + 20 + diameter2;
+	var x2 = x1 + tg;
+	var dy = 15;
+	var img_size = 32;
 
 	var img_1 = g.append("image")
-	   .attr('x', x1 - 20)
-	   .attr('y', height/2 - 20)
-	   .attr('width', 40)
-	   .attr('height', 40)
+	   .attr('x', x1 - img_size/2)
+	   .attr('y', height/2 - img_size/2 + dy)
+	   .attr('width', img_size)
+	   .attr('height', img_size)
 	   .style('opacity', 0.8)
 	   .attr("xlink:href","bus2.png");
 
 	var img_2 = g.append("image")
-	   .attr('x', x2 - 20)
-	   .attr('y', height/2 - 20)
-	   .attr('width', 40)
-	   .attr('height', 40)
-	   .style('opacity', 0.9)
+	   .attr('x', x2 - img_size/2)
+	   .attr('y', height/2 - img_size/2 +dy)
+	   .attr('width', img_size)
+	   .attr('height', img_size)
+	   .style('opacity', 0.92)
 	   .attr("xlink:href","bike2.png");
 
 	var layer_1 = g.append('circle') // blue: bus + walking
 		.attr('cx', x1)
-		.attr('cy', height/2 + 0 )
+		.attr('cy', height/2 + dy)
 		.attr('r', diameter2)
 		.style('fill', '#fff')
 		.style('fill-opacity', 0.15)
@@ -53,11 +59,11 @@ function drawLayers() {
 		.attr('stroke', 'rgba(255,255,255,0.7)')
 		.on("mouseover", function() {
 			d3.select(this).style('fill-opacity', 0.3);
-			tooltip.text("Bus Routes");
-			tooltip.style("visibility", "visible");
+			// tooltip.text("Bus Routes");
+			// tooltip.style("visibility", "visible");
 		})
 		.on("mousemove", function(){
-			tooltip.style("top", (event.pageY- 40)+"px").style("left",(event.pageX -40)+"px");
+			// tooltip.style("top", (event.pageY- 40)+"px").style("left",(event.pageX -40)+"px");
 		})
 		.on("mouseout", function() {
 			d3.select(this).style('fill-opacity', 0.15);
@@ -69,7 +75,7 @@ function drawLayers() {
 
 	var layer_2 = g.append('circle') // blue: bus + walking
 		.attr('cx', x2)
-		.attr('cy', height/2 + 0 )
+		.attr('cy', height/2 + dy )
 		.attr('r', diameter2)
 		.style('fill', '#fff')
 		.style('fill-opacity', 0.15)
@@ -77,11 +83,11 @@ function drawLayers() {
 		.attr('stroke', 'rgba(255,255,255,0.7)')
 		.on("mouseover", function() {
 			d3.select(this).style('fill-opacity', 0.3);
-			tooltip.text("Hubway Station");
-			tooltip.style("visibility", "visible");
+			// tooltip.text("Hubway Station");
+			// tooltip.style("visibility", "visible");
 		})
 		.on("mousemove", function(){
-			tooltip.style("top", (event.pageY- 40)+"px").style("left",(event.pageX -40)+"px");
+			// tooltip.style("top", (event.pageY- 40)+"px").style("left",(event.pageX -40)+"px");
 		})
 		.on("mouseout", function() {
 			d3.select(this).style('fill-opacity', 0.15);
@@ -91,22 +97,46 @@ function drawLayers() {
 			
 		});
 
-	// var layer_3 = g.append('circle') // blue: bus + walking
-	// 	.attr('cx', x3)
-	// 	.attr('cy', height/2 + 5 )
-	// 	.attr('r', diameter2)
-	// 	.style('fill', '#fff')
-	// 	.style('fill-opacity', 0.1)
-	// 	.attr('stroke-width', 1.4)
-	// 	.attr('stroke', 'rgba(255,255,255,0.7)')
-	// 	.on("mouseover", function() {
-	// 		d3.select(this).style('fill-opacity', 0.3);
-	// 	})
-	// 	.on("mouseout", function() {
-	// 		d3.select(this).style('fill-opacity', 0.1);
-	// 	})
-	// 	.on('click', function() {
-	// 	});
+	var text_1 =  g.append('text') // blue: bus + walking
+		.attr('x', x1 + diameter2 + 8)
+		.attr('y',  height/2 + dy)
+		.text('Bus')
+		.attr('class', 'legend2')
+		.style('fill', '#fff')
+		.style('fill-opacity', 0.65)
+		.attr('text-anchor', 'start')
+		.attr('stroke', 'none');
+
+	var text_1_1 =  g.append('text') // blue: bus + walking
+		.attr('x', x1 + diameter2 + 8)
+		.attr('y',  height/2 + dy + 15)
+		.text('Routes')
+		.attr('class', 'legend2')
+		.style('fill', '#fff')
+		.style('fill-opacity', 0.65)
+		.attr('text-anchor', 'start')
+		.attr('stroke', 'none');
+
+	var text_2 =  g.append('text') // blue: bus + walking
+		.attr('x', x2 + diameter2 + 8)
+		.attr('y',  height/2 + dy)
+		.text('Hubway')
+		.attr('class', 'legend2')
+		.style('fill', '#fff')
+		.style('fill-opacity', 0.65)
+		.attr('text-anchor', 'start')
+		.attr('stroke', 'none');
+
+	var text_2_1 =  g.append('text') // blue: bus + walking
+		.attr('x', x2 + diameter2 + 8)
+		.attr('y',  height/2 + dy + 15)
+		.text('Stations')
+		.attr('class', 'legend2')
+		.style('fill', '#fff')
+		.style('fill-opacity', 0.65)
+		.attr('text-anchor', 'start')
+		.attr('stroke', 'none');
+
 }
 
 function drawTimeline() {
